03e3ddfd407572227421eb2b9cf70a051ce004ef
aminebentekaya <57861791+aminebentekaya@users.noreply.github.com>
2020-01-12 09:37:53 +0100

    first commit

---
+++ js/codejava.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    var textarea = document.getElementById("text")
+    textarea.style.fontWeight = 'bold';
+  }
+
+  function myitalicFunction() {
+    var textarea = document.getElementById("text")
+    textarea.style.fontstyle = 'italic';  }
+
+  function italicText(){
+    var target = document.getElementById("TextArea");
+    if( target.style.fontStyle == "italic" ) {
+        target.style.fontStyle = "normal";
+    } else {
+        target.style.fontStyle = "italic";
+    }
+}
+
+
+  function myunderlineFunction() {
+    var textarea = document.getElementById("text")
+    textarea.style.fontWeight = 'bold';
+
+  }
